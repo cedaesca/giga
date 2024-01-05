89110b224ec78bcec321c2a611faed75a3753736
Cesar Escudero <cedaesca@gmail.com>
2024-01-05 12:50:26 -0400

    feat: add bootstrap styling

--- resources/views/dog/create.blade.php
+++ resources/views/dog/create.blade.php
@@ -4,33 +4,47 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
-    @if($errors->any())
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-    @endif
-    @if (session('success'))
-        <div>
-            {{ session('success') }}
+    <div class="container">
+        <div class="row form-container">
+            <div class="col-md-6 offset-md-3">
+                @if($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+
+                @if (session('success'))
+                    <div class="alert alert-success">
+                        {{ session('success') }}
+                    </div>
+                @endif
+
+                <form action="{{ route('dogs.store') }}" method="post" class="card p-4">
+                    @csrf
+                    <div class="form-group">
+                        <label for="name">Dog's name:</label>
+                        <input name="name" type="text" class="form-control" value="{{ old('name') }}">
+                    </div>
+                    <div class="form-group">
+                        <label for="birth_date">Dog's birth date:</label>
+                        <input id="birth_date" name="birth_date" type="date" class="form-control" value="{{ old('birth_date') }}">
+                    </div>
+                    <div class="form-check">
+                        <input type="hidden" name="is_birth_date_exact" value="0">
+                        <input id="is_birth_date_exact" type="checkbox" name="is_birth_date_exact" class="form-check-input" value="1" {{ old('is_birth_date_exact') ? 'checked' : '' }}>
+                        <label class="form-check-label" for="is_birth_date_exact">Is the dog's birth date exact?</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary mt-3">Submit</button>
+                </form>
+            </div>
         </div>
-    @endif
-    <form action="{{ route('dogs.store') }}" method="post">
-        @csrf
-        <label id="dog_name" for="name">Dog's name:</label>
-        <input name="name" type="text" value="{{ old('name') }}">
-        <br>
-        <label for="birth_date">Dog's birth date:</label>
-        <input id="dog_birth_date" name="birth_date" type="date" value="{{ old('birth_date') }}">
-        <br>
-        <label for="is_birth_date_exact">Is the dog's birth date exact?</label>
-        <input type="hidden" name="is_birth_date_exact" value="0">
-        <input type="checkbox" name="is_birth_date_exact" value="1" {{ old('is_birth_date_exact') ? 'checked' : '' }}>
-        <br>
-        <input type="submit">
-    </form>
+    </div>
 </body>
 </html>
