fdbef874501deaa9b317a8fd4351b4f8b6cfeec7
César Escudero <cedaesca@gmail.com>
2024-01-05 15:09:56 -0400

    tests: dog controller (#6)
    * feat: use in-memory db
    
    * feat: add test cases for dog controller

---
+++ database/factories/DogFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Dog>
+ */
+class DogFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name,
+            'birth_date' => now(),
+            'is_birth_date_exact' => fake()->boolean()
+        ];
+    }
+}
